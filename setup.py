c97f2cee2e42d6239f473626f36caa4ffe67ae9e
Heyaw Meteke <heyawmeteke@gmail.com>
2016-05-11 16:28:11 -0700

    fixed setup.py
    added brew instead of easy install

--- setup.py
+++ setup.py
@@ -13,4 +13,4 @@ for x in range(0,2):
 		os.system('sudo pip install pyinstaller')
 		os.system('pyinstaller --onefile cl.py')
 	except:
-		os.system('sudo easy_install pip')
+		os.system('sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
