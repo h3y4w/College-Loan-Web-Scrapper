6a56bfce89f695e27202671543b449dabe1005b3
Heyaw Meteke <heyawmeteke@gmail.com>
2016-05-11 16:31:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 import os
 
 #########################
-#			#
+#LAZY			#
 #EASY SETUP FILE 	#
 #AUTOMATICALLY DELETES	#
 #AFTER SUCCESSFUL	#
 #INSTALL		#
 #########################
-for x in range(0,2):
+
+
+os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+os.system('sudo pip install pyinstaller')
+os.system('pyinstaller --onefile cl.py')
 	
-	try:
-		os.system('sudo pip install pyinstaller')
-		os.system('pyinstaller --onefile cl.py')
-	except:
-		os.system('sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
